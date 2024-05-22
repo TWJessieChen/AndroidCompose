252e37b36e935517be03d71b5313ef002e3d986f
JC666 <jessie.pc.chen@viewsonic.com>
2024-05-22 15:54:36 +0800

    1.新增帶入陣列使用迴圈方式產生多個ui 2.修改ui更像list樣式

--- app/src/main/java/com/jc666/androidcompose/MainActivity.kt
+++ app/src/main/java/com/jc666/androidcompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -42,12 +43,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Android")
+fun MyApp(modifier: Modifier = Modifier,
+          names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = modifier.padding(4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
@@ -56,15 +57,16 @@ fun MyApp(modifier: Modifier = Modifier) {
  */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = modifier.padding(24.dp)) {
+    Surface(color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
             Text(text = "Hello ")
             Text(text = name)
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     AndroidComposeTheme {
