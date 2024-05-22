82b4d465aae66f84819c609e3131fc91aac25cf4
JC666 <jessie.pc.chen@viewsonic.com>
2024-05-22 16:36:05 +0800

    1.新增mutableStateOf方式紀錄狀態 2.新增remember保護expanded不受重組影響，讓系統不會重設狀態，要不然每次都會重置狀態，導致無法紀錄

--- app/src/main/java/com/jc666/androidcompose/MainActivity.kt
+++ app/src/main/java/com/jc666/androidcompose/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,8 +52,8 @@ fun MyApp(modifier: Modifier = Modifier,
     Column(modifier = modifier.padding(4.dp)) {
         for (name in names) {
             Greeting(name = name)
-            Greeting2(name = name)
-            Greeting3(name = name)
+//            Greeting2(name = name)
+//            Greeting3(name = name)
         }
     }
 }
@@ -61,6 +63,8 @@ fun MyApp(modifier: Modifier = Modifier,
  */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
@@ -69,9 +73,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = {  }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
