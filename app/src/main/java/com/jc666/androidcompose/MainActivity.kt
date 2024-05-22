8244707c02bef0fcad610f18f248a09b8152ccd6
PO-CHUN CHEN <>
2024-05-22 21:58:14 +0800

    1.新增OnboardingScreen頁面 2.新增Clicked點擊callback fun

--- app/src/main/java/com/jc666/androidcompose/MainActivity.kt
+++ app/src/main/java/com/jc666/androidcompose/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.jc666.androidcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jc666.androidcompose.ui.theme.AndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
+    private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -46,10 +53,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun MyApp(modifier: Modifier = Modifier,
-          names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = modifier.padding(4.dp)) {
+fun GreetingPreview() {
+    AndroidComposeTheme {
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    AndroidComposeTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Log.d("MyApp", "MyApp")
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
 //            Greeting2(name = name)
@@ -87,6 +126,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
@@ -113,13 +172,5 @@ fun Greeting3(name: String, modifier: Modifier = Modifier) {
             Text(text = "Hello ")
             Text(text = name)
         }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun GreetingPreview() {
-    AndroidComposeTheme {
-        MyApp()
     }
 }
