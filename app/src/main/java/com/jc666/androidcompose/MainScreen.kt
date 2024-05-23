4fabe1724910fc387ee3412ea2d112ac60897eb1
JC666 <jessie.pc.chen@viewsonic.com>
2024-05-23 16:10:16 +0800

    1.修改使用LazyColumn(RecyclerView)

--- app/src/main/java/com/jc666/androidcompose/MainScreen.kt
+++ app/src/main/java/com/jc666/androidcompose/MainScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -56,15 +58,20 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
-//            Greeting2(name = name)
-//            Greeting3(name = name)
         }
     }
+//    Column(modifier = modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name = name)
+////            Greeting2(name = name)
+////            Greeting3(name = name)
+//        }
+//    }
 }
 
 /**
